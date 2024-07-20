d98ed8e5cf2de24b10511e137eae7677f1130d62
CoolShivas <shivas2710cool00@gmail.com>
2024-07-20 20:42:41 +0530

    First form of Pre-Rendering or Getting data with the help of useState and useEffect Hook.

--- meetups_project/pages/index.js
+++ meetups_project/pages/index.js
@@ -1,33 +1,43 @@
+import { useEffect, useState } from "react";
 import MeetUpList from "../components/meetUps/MeetUpList";
 
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "A First MeetUp.",
+    image: "https://prasadyash2411.github.io/ecom-website/img/Album%201.png",
+    address: "Soni Coloney, Choudhary Mohalla, Guna(M.P.)",
+    description: "Great and amazing place to enjoy.",
+  },
+  {
+    id: "m2",
+    title: "The Second MeetUp.",
+    image: "https://prasadyash2411.github.io/ecom-website/img/Album%203.png",
+    address: "Dubey Coloney, Choudhary Mohalla, Guna(M.P.)",
+    description: "Better to best to hangout.",
+  },
+  {
+    id: "m3",
+    title: "The Third MeetUp.",
+    image: "https://prasadyash2411.github.io/ecom-website/img/Album%202.png",
+    address: "Karnel Gunj, Khateek Mohalla, Guna(M.P.)",
+    description: "Places of place to explore more and more everyday.",
+  },
+];
+
 const HomePage = () => {
-  const DUMMY_MEETUPS = [
-    {
-      id: "m1",
-      title: "A First MeetUp.",
-      image: "https://prasadyash2411.github.io/ecom-website/img/Album%201.png",
-      address: "Soni Coloney, Choudhary Mohalla, Guna(M.P.)",
-      description: "Great and amazing place to enjoy.",
-    },
-    {
-      id: "m2",
-      title: "The Second MeetUp.",
-      image: "https://prasadyash2411.github.io/ecom-website/img/Album%203.png",
-      address: "Dubey Coloney, Choudhary Mohalla, Guna(M.P.)",
-      description: "Better to best to hangout.",
-    },
-    {
-      id: "m3",
-      title: "The Third MeetUp.",
-      image: "https://prasadyash2411.github.io/ecom-website/img/Album%202.png",
-      address: "Karnel Gunj, Khateek Mohalla, Guna(M.P.)",
-      description: "Places of place to explore more and more everyday.",
-    },
-  ];
+  const [loadedMeetUps, setLoadedMeetUps] = useState([]);
+
+  useEffect(() => {
+    // Send a http request and fetch data ;
+    setLoadedMeetUps(DUMMY_MEETUPS);
+  }, []);
 
   return (
     <>
-      <MeetUpList meetups={DUMMY_MEETUPS}></MeetUpList>
+      {/* <MeetUpList meetups={DUMMY_MEETUPS}></MeetUpList> */}
+
+      <MeetUpList meetups={loadedMeetUps}></MeetUpList>
     </>
   );
 };
