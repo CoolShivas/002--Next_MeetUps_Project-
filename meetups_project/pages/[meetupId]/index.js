ed904d1f3afe7cb222c5cf865ba841bf41c43e84
CoolShivas <shivas2710cool00@gmail.com>
2024-07-20 13:23:06 +0530

    Formation of [meetupid] Page with content in it.

--- meetups_project/pages/[meetupId]/index.js
+++ meetups_project/pages/[meetupId]/index.js
@@ -1,7 +1,18 @@
 const MeetUpId = () => {
   return (
     <>
-      <h1> Welcome to the meetup Ids .</h1>
+      <img
+        src="https://prasadyash2411.github.io/ecom-website/img/Album%201.png"
+        alt="image not found"
+      />
+      <h1>A First MeetUp.</h1>
+      <address>Soni Coloney, Choudhary Mohalla, Guna(M.P.)</address>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem ea
+        inventore quibusdam laboriosam ipsa labore excepturi, minima alias cum
+        recusandae sapiente optio, vel odit ipsam?
+      </p>
+      {/* This page can be reach out by writing in URL http://localhost:3000/shiva or http://localhost:3000/anything  */}
     </>
   );
 };
