432b1b6d41d027f7d8a8cf6adc046663bea33cd7
CoolShivas <shivas2710cool00@gmail.com>
2024-07-20 09:37:31 +0530

    Formation of Pages and Folders.

---
+++ meetups_project/pages/[meetupId]/index.js
@@ -0,0 +1,9 @@
+const MeetUpId = () => {
+  return (
+    <>
+      <h1> Welcome to the meetup Ids .</h1>
+    </>
+  );
+};
+
+export default MeetUpId;
---
+++ meetups_project/pages/newmeetups/index.js
@@ -0,0 +1,9 @@
+const NewMeetUps = () => {
+  return (
+    <>
+      <h1> Here, you can see the new meetups. </h1>
+    </>
+  );
+};
+
+export default NewMeetUps;
