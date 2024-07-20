ddd4c7acf9e6b7187949dd10f37a911dafce7beb
CoolShivas <shivas2710cool00@gmail.com>
2024-07-20 10:17:39 +0530

    Formation of Components and its folder with MeetUpList and MeetUpItem and Dummy data.

--- meetups_project/pages/index.js
+++ meetups_project/pages/index.js
@@ -1,7 +1,33 @@
+import MeetUpList from "../components/meetUps/MeetUpList";
+
 const HomePage = () => {
+  const DUMMY_MEETUPS = [
+    {
+      id: "m1",
+      title: "A First MeetUp.",
+      image: "https://prasadyash2411.github.io/ecom-website/img/Album%201.png",
+      address: "Soni Coloney, Choudhary Mohalla, Guna(M.P.)",
+      description: "Great and amazing place to enjoy.",
+    },
+    {
+      id: "m2",
+      title: "The Second MeetUp.",
+      image: "https://prasadyash2411.github.io/ecom-website/img/Album%203.png",
+      address: "Dubey Coloney, Choudhary Mohalla, Guna(M.P.)",
+      description: "Better to best to hangout.",
+    },
+    {
+      id: "m3",
+      title: "The Third MeetUp.",
+      image: "https://prasadyash2411.github.io/ecom-website/img/Album%202.png",
+      address: "Karnel Gunj, Khateek Mohalla, Guna(M.P.)",
+      description: "Places of place to explore more and more everyday.",
+    },
+  ];
+
   return (
     <>
-      <h1> Welcome to the Home Page.</h1>
+      <MeetUpList meetups={DUMMY_MEETUPS}></MeetUpList>
     </>
   );
 };
