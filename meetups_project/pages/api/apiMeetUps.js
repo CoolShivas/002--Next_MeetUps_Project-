fe0373f106349fbbc0050b331733607d4228faab
CoolShivas <shivas2710cool00@gmail.com>
2024-07-21 17:24:53 +0530

    Installation of mongodb. And with the help of MongoClient connecting to the mongo server with route name shivajiData and making collection i.e, uniqueStar and insertation of data to it.

---
+++ meetups_project/pages/api/apiMeetUps.js
@@ -0,0 +1,21 @@
+import { MongoClient } from "mongodb";
+
+const apiMeetUps = async (req, res) => {
+  if (req.method === "POST") {
+    const data = req.body;
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://shivas2710cool00:8frqrkDg0VDOu0MK@cluster0.ncqmpot.mongodb.net/shivajiData?retryWrites=true&w=majority&appName=Cluster0"
+    );
+
+    const db = client.db();
+
+    const meetupsCollection = db.collection("uniqueStar");
+
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+  }
+};
+
+export default apiMeetUps;
