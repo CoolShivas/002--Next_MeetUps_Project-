9da3c8d47a54450527a29f609d477bce444b1b58
CoolShivas <shivas2710cool00@gmail.com>
2024-07-22 14:22:12 +0530

    Getting Meetup Details Data & Preparing Pages i.e, making the Show Detail btn data dynamic instead of static. Now, Show Detail btn will show the details of that meetups only not static data.

--- meetups_project/pages/[meetupId]/index.js
+++ meetups_project/pages/[meetupId]/index.js
@@ -1,6 +1,78 @@
+// import MeetUpDetails from "../../components/meetups/MeetUpDetails";
+
+// const MeetUpId = () => {
+//   return (
+//     <>
+//       {/* <img
+//         src="https://prasadyash2411.github.io/ecom-website/img/Album%201.png"
+//         alt="image not found"
+//       />
+//       <h1>A First MeetUp.</h1>
+//       <address>Soni Coloney, Choudhary Mohalla, Guna(M.P.)</address>
+//       <p>
+//         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem ea
+//         inventore quibusdam laboriosam ipsa labore excepturi, minima alias cum
+//         recusandae sapiente optio, vel odit ipsam?
+//       </p> */}
+
+//       {/* This page can be reach out by writing in URL http://localhost:3000/shiva or http://localhost:3000/anything  */}
+
+//       <MeetUpDetails
+//         image="https://prasadyash2411.github.io/ecom-website/img/Album%201.png"
+//         title="A First MeetUp."
+//         address="Soni Coloney, Choudhary Mohalla, Guna(M.P.)"
+//         description="This is the great place."
+//       ></MeetUpDetails>
+//       {/* Passing the static data to MeetUpDetails not dynamic data; */}
+//     </>
+//   );
+// };
+
+// export async function getStaticPaths() {
+//   return {
+//     fallback: true,
+//     paths: [
+//       {
+//         params: {
+//           meetupId: "m1",
+//         },
+//       },
+//       {
+//         params: {
+//           meetupId: "m2",
+//         },
+//       },
+//     ],
+//   };
+// }
+
+// export async function getStaticProps(context) {
+//   // Fetch data for a single meetup ;
+
+//   const meetingId = context.params.meetupId;
+
+//   return {
+//     props: {
+//       meetupdata: {
+//         id: meetingId,
+//         image:
+//           "https://prasadyash2411.github.io/ecom-website/img/Album%201.png",
+//         title: "A First MeetUp.",
+//         address: "Soni Coloney, Choudhary Mohalla, Guna(M.P.)",
+//         description: "This is the great place.",
+//       },
+//     },
+//   };
+// }
+
+// export default MeetUpId;
+
+///******************************************************************************************************************* */
+
+import { MongoClient, ObjectId } from "mongodb";
 import MeetUpDetails from "../../components/meetups/MeetUpDetails";
 
-const MeetUpId = () => {
+const MeetUpId = (props) => {
   return (
     <>
       {/* <img
@@ -18,10 +90,14 @@ const MeetUpId = () => {
       {/* This page can be reach out by writing in URL http://localhost:3000/shiva or http://localhost:3000/anything  */}
 
       <MeetUpDetails
-        image="https://prasadyash2411.github.io/ecom-website/img/Album%201.png"
-        title="A First MeetUp."
-        address="Soni Coloney, Choudhary Mohalla, Guna(M.P.)"
-        description="This is the great place."
+        // image="https://prasadyash2411.github.io/ecom-website/img/Album%201.png"
+        // title="A First MeetUp."
+        // address="Soni Coloney, Choudhary Mohalla, Guna(M.P.)"
+        // description="This is the great place."
+        image={props.dataFromServer.image}
+        title={props.dataFromServer.title}
+        address={props.dataFromServer.address}
+        description={props.dataFromServer.description}
       ></MeetUpDetails>
       {/* Passing the static data to MeetUpDetails not dynamic data; */}
     </>
@@ -29,37 +105,77 @@ const MeetUpId = () => {
 };
 
 export async function getStaticPaths() {
+  const client = await MongoClient.connect(
+    "mongodb+srv://shivas2710cool00:8frqrkDg0VDOu0MK@cluster0.ncqmpot.mongodb.net/shivajiData?retryWrites=true&w=majority&appName=Cluster0"
+  );
+
+  const db = client.db();
+
+  const meetupsCollection = db.collection("uniqueStar");
+
+  const result = await meetupsCollection.find({}, { _id: 1 }).toArray();
+
+  client.close();
+
   return {
-    fallback: true,
-    paths: [
-      {
-        params: {
-          meetupId: "m1",
-        },
-      },
-      {
-        params: {
-          meetupId: "m2",
-        },
-      },
-    ],
+    fallback: false,
+    // paths: [
+    //   {
+    //     params: {
+    //       meetupId: "m1",
+    //     },
+    //   },
+    //   {
+    //     params: {
+    //       meetupId: "m2",
+    //     },
+    //   },
+    // ],
+    paths: result.map((brr) => ({
+      params: { meetupId: brr._id.toString() },
+    })),
   };
 }
 
 export async function getStaticProps(context) {
   // Fetch data for a single meetup ;
 
-  const meetingId = context.params.meetupId;
+  const meetupId = context.params.meetupId;
+
+  const client = await MongoClient.connect(
+    "mongodb+srv://shivas2710cool00:8frqrkDg0VDOu0MK@cluster0.ncqmpot.mongodb.net/shivajiData?retryWrites=true&w=majority&appName=Cluster0"
+  );
+
+  const db = client.db();
+
+  const meetupsCollection = db.collection("uniqueStar");
+
+  const selectedMeetUp = await meetupsCollection.findOne({
+    _id: new ObjectId(meetupId),
+  });
+
+  console.log(selectedMeetUp);
+
+  client.close();
 
   return {
+    // props: {
+    //   meetupdata: {
+    //     id: meetingId,
+    //     image:
+    //       "https://prasadyash2411.github.io/ecom-website/img/Album%201.png",
+    //     title: "A First MeetUp.",
+    //     address: "Soni Coloney, Choudhary Mohalla, Guna(M.P.)",
+    //     description: "This is the great place.",
+    //   },
+    // },
     props: {
-      meetupdata: {
-        id: meetingId,
-        image:
-          "https://prasadyash2411.github.io/ecom-website/img/Album%201.png",
-        title: "A First MeetUp.",
-        address: "Soni Coloney, Choudhary Mohalla, Guna(M.P.)",
-        description: "This is the great place.",
+      dataFromServer: {
+        id: selectedMeetUp._id.toString(),
+        title: selectedMeetUp.title,
+        address: selectedMeetUp.address,
+        image: selectedMeetUp.image,
+        description: selectedMeetUp.description,
       },
     },
   };
